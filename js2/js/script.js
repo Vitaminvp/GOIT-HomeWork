72a75d47c29b144110fc4cba1e946bbfd73d7cbb
Vitamin <Vitamin@ukr.net>
2017-05-29 19:20:19 +0300

    Домашка js 1-2.1

--- js2/js/script.js
+++ js2/js/script.js
@@ -1,23 +1,20 @@
-// alert('hello');
-
-
 function myArr() {
   var arrey = [];
   for (var i=0; i < 5; i++) {
     arrey[i] = prompt('Введите '+(i+1)+'-й елемент массива.' );
-    // console.log(arrey[i]);
+    if (arrey[i] == '') {
+      arrey[i] = prompt('Нужно чтото ввести.' );
+    }
   }
   return arrey;
 }
 
 var arr = myArr();
-// console.log(arr);
 
 function compare(name) {
   for (var i = 0; i < arr.length; i++) {
-    // console.log(arr[i]);
     var enter;
-      if (arr[i] == name) {
+      if ( (arr[i] == name) & (arr[i] !==  '') ) {
         enter = true;
         break;
       } else {
@@ -28,6 +25,7 @@ function compare(name) {
     alert(name + ', Вы вошли.');
   } else {
     alert('Error.');
+
   }
 }
 compare(name = prompt( 'Введите имя пользователя.' ));
